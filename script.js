59c6a46b6109eaeb9c5108c125bf0e687982786a
mahmoudkhaleddev <75860403+mahmoudkhaleddev@users.noreply.github.com>
2023-07-14 06:37:02 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date-container").innerHTML = "Wed , " + m + " - " + d + " - " + y;
+
+
+
